86ca79352b867fae23b3e36d19bb21e01b23186f
rachelle-d <76711245+rachelle-d@users.noreply.github.com>
2021-10-05 16:51:32 -0400

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,10 @@
+simple js application, information gathered from open weather map API
+
+Usage: could be run with "live server" in VSCode.
+
+Features: 
+- support country code, simply add the 2-letter country code after the name of the city, seperate with a comma. eg. Toronto, CA
+- search for any cities around the world!
+- beautiful UI
+- error message for wrong spelling of the cities
+- error message for duplicate cities
---
+++ script.js
@@ -0,0 +1,80 @@
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+const apiKey = "2e84d4b8e562a4cc714d0dfd68cb280e";
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+  //check if city already exists
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      if (inputVal.includes(",")) {
+        //check validity for country codes
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `The weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } is already displaying, different city? try adding a country code.`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  //ajax connection
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Double check the spelling?";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
